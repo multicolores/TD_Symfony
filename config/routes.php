a493e1c2097e105b10ddc30c4f1a41798f9e9dca
Florian TELLIER <florian.tellier02@gmail.com>
2022-10-15 17:55:48 +0200

    exo1 route with php

--- config/routes.php
+++ config/routes.php
@@ -1,10 +1,11 @@
 <?php
-// use App\Controller\Client;
-// use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
-// return function (RoutingConfigurator $routes) {
-//     $routes->add('api_client', '/client/prenom/{prenom}')
-//         ->controller([Client::class, 'info'])
-//         ->methods(['GET', 'HEAD'])
-//     ;
-// };
+use App\Controller\Client;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
+
+return function (RoutingConfigurator $routes) {
+    $routes->add('api_client', '/client/prenom/{prenom}')
+        ->controller([Client::class, 'info'])
+        ->methods(['GET', 'HEAD'])
+        ->requirements(['prenom' => '([a-zA-Z]+)(-[a-zA-Z]+)*']);
+};
--- src/Kernel.php
+++ src/Kernel.php
@@ -4,8 +4,18 @@ namespace App;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
+use Symfony\Component\Routing\Loader\Configurator\RoutingConfigurator;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function configureRoutes(RoutingConfigurator $routes)
+    {
+        $extentions = '{php, xml}';
+
+        $routes->import('../config/{routes}' . $this->environment . "/*.$extentions");
+        $routes->import("../config/{routes}/*.$extentions");
+        $routes->import("../config/{routes}.$extentions");
+    }
 }
