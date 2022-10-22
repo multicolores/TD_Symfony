24b1389c8de7f178204fb45299115ba56cc7a32e
Florian TELLIER <florian.tellier02@gmail.com>
2022-10-22 18:18:43 +0200

    TD4.2

--- src/Controller/FormulaireController.php
+++ src/Controller/FormulaireController.php
@@ -10,6 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Validator\Constraints as Assert;
 
 class FormulaireController extends AbstractController
 {
@@ -18,19 +19,38 @@ class FormulaireController extends AbstractController
      */
     public function new(Request $request): Response
     {
+
+        // http://localhost:8000/TD4/login
+        // changer dans translation.yaml default en fr/en
         $user = new User();
+        $message = '';
+        if ($request->getLocale() == 'fr') {
+            $message = 'Le mot de passe doit avoir au minimum 8 charactère et au moins une lettre et un nombre';
+        } else {
+            $message = 'Password must have a minimum of eight characters, at least one letter and one number';
+        }
+        // $local = $request->getLocale();
+        // dump($local);
         $errorState = false;
         $form = $this->createFormBuilder($user)
             ->add('email', TextType::class)
-            ->add('password', TextType::class)
-            ->add('password2', TextType::class, ['label' => 'Confirm password'])
+            ->add('password', TextType::class, [
+                'constraints' => new Assert\Regex([
+                    'pattern' => '/^(?=.*[A-Za-z])(?=.*\d)[A-Za-z\d]{8,}$/',
+                    'message' => $message
+                ]),
+            ])
+            ->add('password2', TextType::class, [
+                'label' => 'Confirm password',
+            ])
             ->add('save', SubmitType::class, ['label' => 'Create User'])
             ->getForm();
 
+            
         $form->handleRequest($request);
         if ($form->isSubmitted() && $form->isValid()) {
 
-            if($form["password"]->getData() === $form["password2"]->getData()){
+            if ($form["password"]->getData() === $form["password2"]->getData()) {
                 return $this->redirectToRoute('login_success');
             } else {
                 $errorState = true;
